5720ff3913c39d457078f3436918c5bd6d5fd48b
Ashik <ashikur1993@gmail.com>
2025-09-23 23:20:55 +0600

    Update Treatment Results

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,9 +1,11 @@
+import MyAchievements from "@/components/MyAchievements/MyAchievements";
+import SubpageBanner from "@/components/SubpageBanner/SubpageBanner";
+
 export default function Gallery() {
     return (
-        <main>
-            <h2>Gallery</h2>
-            <h2>Gallery</h2>
-            <h2>Gallery</h2>
-        </main>
+        <>
+            <SubpageBanner />
+            <MyAchievements />
+        </>
     );
 }
