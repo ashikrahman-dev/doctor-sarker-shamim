902e93ce1e3561dc80933bd520b35be58efe55c5
Ashik <ashikur1993@gmail.com>
2025-10-11 11:27:49 +0600

    Contact page responsive

--- components/ContactSection/ContactSection.jsx
+++ components/ContactSection/ContactSection.jsx
@@ -8,18 +8,21 @@ const contactInfo = [
         icon: "/images/phone-call.svg",
         title: "Phone",
         info: "+41911234567",
+        link: "tel:+41911234567",
     },
     {
         id: 2,
         icon: "/images/envelope.svg",
         title: "Email",
         info: "info@acquathermic.ch",
+        link: "mailto:info@acquathermic.ch",
     },
     {
         id: 3,
         icon: "/images/printer.svg",
         title: "Fax",
         info: "+41 91 123 45 67",
+        
     },
     ,
     {
@@ -36,15 +39,14 @@ export default function ContactSection() {
             {/* Map */}
             <div className="container">
                 {/* Get In Touch -start */}
-                <div className="rounded-4xl bg-white grid grid-cols-1 md:grid-cols-2 gap-[30px] min-h-[650px]">
-                    <div className="px-4 lg:px-24 py-6 lg:py-[100px] max-w-[700px] bg-gradient-to-r from-white to-[#ffffff31] rounded-4xl">
+                <div className="rounded-4xl bg-white grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-[30px] lg:min-h-[500px] 2xl:min-h-[650px]">
+                    <div className="px-8 lg:px-10 xl:px-24 py-8 lg:py-10 2xl:py-[100px] max-w-[700px] bg-gradient-to-r from-white to-[#ffffff31] rounded-4xl">
                         <div className="text-light-foreground self-stretch font-normal text-2xl lg:text-3xl xl:text-4xl font-anton  leading-[1.3] mb-4">
                             Contact Us
                         </div>
                         <p className="font-normal text-sm sm:text-base text-light-foreground/70">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.
+                            elit.
                         </p>
                     </div>
                     <div>
@@ -67,9 +69,9 @@ export default function ContactSection() {
                 {/* Get In Touch -end */}
 
                 {/* Contact Form */}
-                <div className="grid grid-cols-12 gap-6 -mt-[287px]">
-                    <div className="col-span-10 col-start-2 bg-[#26292A] p-10 rounded-4xl">
-                        <div className="flex gap-7.5">
+                <div className="grid grid-cols-12 gap-6 mt-12 md:-mt-16 lg:-mt-[287px]">
+                    <div className="col-span-12 md:col-span-10 md:col-start-2 bg-[#26292A] px-6 py-8  sm:p-8 md:p-10 rounded-2xl lg:rounded-3xl 2xl:rounded-4xl">
+                        <div className="flex flex-wrap lg:flex-nowrap gap-10 lg:gap-7.5">
                             <div className="w-full">
                                 <div className="text-white self-stretch font-normal text-2xl lg:texttext-[30px] font-anton leading-[1.3] mb-5">
                                     Contact Us
@@ -86,14 +88,14 @@ export default function ContactSection() {
                                             className="bg-white/5 rounded-xl p-2 flex gap-5 items-center"
                                         >
                                             {/* Icon */}
-                                            <div className="w-21 h-21">
-                                                <div className="w-21 h-21 rounded-lg bg-white/7 flex items-center justify-center">
+                                            <div className="w-16 lg:w-21 h-16 lg:h-21">
+                                                <div className="w-16 lg:w-21 h-16 lg:h-21 rounded-lg bg-white/7 flex items-center justify-center">
                                                     <Image
                                                         src={item?.icon}
                                                         alt={item?.title}
                                                         width={24}
                                                         height={24}
-                                                        className="w-6 h-6"
+                                                        className="w-5 lg:w-6 h-5 lg:h-6"
                                                     />
                                                 </div>
                                             </div>
@@ -103,8 +105,8 @@ export default function ContactSection() {
                                                     {item?.title}
                                                 </p>
                                                 <Link
-                                                    href="tel:+41911234567"
-                                                    className="text-white/90 text-lg font-medium tracking-[0.09]"
+                                                    href={item?.link ? item?.link : "#"}
+                                                    className="text-white/90 text-sm sm:text-base md:text-lg font-medium tracking-[0.09]"
                                                 >
                                                     {item?.info}
                                                 </Link>
